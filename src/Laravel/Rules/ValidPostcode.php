7e83689bcc61ac1e1143191e732a4d182724974b
Ryan Chandler <ryangjchandler@gmail.com>
2024-04-26 14:56:31 +0100

    feature: laravel 11 and valid postcode rule

---
+++ src/Laravel/Rules/ValidPostcode.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace C6Digital\MapIt\Laravel\Rules;
+
+use C6Digital\MapIt\MapIt;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class ValidPostcode implements ValidationRule
+{
+    public function __construct(
+        protected MapIt $mapIt,
+    ) {}
+
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $result = $this->mapIt->postcode($value);
+
+        if ($result === null || isset($result['error'])) {
+            $fail('The :attribute must be a valid postcode.');
+        }
+    }
+}
